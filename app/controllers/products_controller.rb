9939da9dba2c8006a8baa622e2983ed57ab26879
MELINA MEHR <MELINAMEHR@MSN.COM>
2017-06-14 13:46:13 -0400

    created controller skeleton

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,24 @@
+class ProductsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def destory
+  end
+
+  def update
+  end
+
+end
