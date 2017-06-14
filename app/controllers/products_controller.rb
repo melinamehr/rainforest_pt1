49225f3deff0e3f7215d9d8d0444cedd8a15e869
Doug <doug.steinberg@gmail.com>
2017-06-14 16:44:39 -0400

    Able to create new product. Redirects to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,12 +13,18 @@ class ProductsController < ApplicationController
   end
 
   def create
-    @product = Product.new(params[:id])
+    @product = Product.new(product_params)
     if @product.save
       redirect_to "/products"
     else
       render :new
     end
+  end
+
+private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price_in_cents)
   end
 
   def edit
