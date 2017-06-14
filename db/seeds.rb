4b3be6f533f0bd72e9681063e4ddf9c19fe7100e
Doug <doug.steinberg@gmail.com>
2017-06-14 14:55:15 -0400

    Index and show pages working correctly

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,15 +4,16 @@ Product.create(
   topped with an Earl Grey infused glaze',
   price_in_cents: 350
 )
-
+puts "..."
 Product.create(
   name:  'Tuna Melt Sandwich',
   description: 'Sharp cheddar, tuna salad on panini',
   price_in_cents: 499
 )
-
+puts "..."
 Product.create(
   name:  'Strawberry Milkshake',
   description: 'Locally picked strawberries blended in an creme de fraiche',
   price_in_cents: 399
 )
+puts "..."
