2bd7aa380d86bb7f4ff0d913cf1c175d3a713088
Doug <doug.steinberg@gmail.com>
2017-06-15 18:30:16 -0400

    Review create method not working properly. Added new, destroy methods in reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,35 @@
 class ReviewsController < ApplicationController
-  def show
-    
+
+  def new
+    @review = Product.find(params[:product_id]).reviews.new
+  end
+  # @review = @product.reviews.build
+
+  def create
+    @review = Review.new(review_params.merge({product_id:params[:product_id]}))  #.merge to join w past comments?
+    if @review.save
+      redirect_to product_path(@review.product)
+    else
+      :render 'products'
+    end
+  end
+
+  # def edit
+  #   @review = Review.find(params[:id])
+  # end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to product_path(@review.product)
+
+  end
+
+
+  private
+
+  def review_params
+    params.require(:review).permit(:comment)
   end
 
 end
