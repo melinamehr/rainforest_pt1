77fa1e5d9285b9e836c5a4f52cc70dd807250f5e
Doug <doug.steinberg@gmail.com>
2017-06-15 16:55:17 -0400

    Added nested routes for reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,34 @@
-Product.create(
-  name:  'London Fog Donut',
-  description: 'A yeast doughnut filled with an Earl Grey pastry cream
-  topped with an Earl Grey infused glaze',
-  price_in_cents: 350
+# Product.create(
+#   name:  'London Fog Donut',
+#   description: 'A yeast doughnut filled with an Earl Grey pastry cream
+#   topped with an Earl Grey infused glaze',
+#   price_in_cents: 350
+# )
+# puts "..."
+# Product.create(
+#   name:  'Tuna Melt Sandwich',
+#   description: 'Sharp cheddar, tuna salad on panini',
+#   price_in_cents: 499
+# )
+# puts "..."
+# Product.create(
+#   name:  'Strawberry Milkshake',
+#   description: 'Locally picked strawberries blended in an creme de fraiche',
+#   price_in_cents: 399
+# )
+# puts "..."
+# Product.create(
+#   name:  'Strawberry Milkshake',
+#   description: 'Locally picked strawberries blended in an creme de fraiche',
+#   price_in_cents: 399
+# )
+# puts "..."
+Review.create(
+  comment:  'Delicious! Highly recommended for anyone and everyone. Will definitely be having again.',
+  product_id: Product.first.id
 )
-puts "..."
-Product.create(
-  name:  'Tuna Melt Sandwich',
-  description: 'Sharp cheddar, tuna salad on panini',
-  price_in_cents: 499
+
+Review.create(
+  comment:  'So fishy! Would have liked some more salt n peppa',
+  product_id: Product.second.id
 )
-puts "..."
-Product.create(
-  name:  'Strawberry Milkshake',
-  description: 'Locally picked strawberries blended in an creme de fraiche',
-  price_in_cents: 399
-)
-puts "..."
